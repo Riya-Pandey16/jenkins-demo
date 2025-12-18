26ac4dfe2033f155cb936f86448d1cc3392c90f0
Your GitHub Username <yourgithubemail@gmail.com>
2025-12-18 18:18:04 +0530

    simple scripts to practice jenkins

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js CI pipeline!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
